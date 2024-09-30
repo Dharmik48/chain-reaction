c91243de1804333a9c201ef44236636c440cf736
dharmik48 <hingudharmik4@gmail.com>
2024-09-30 23:13:53 +0530

    feat: adds more players

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,5 +4,9 @@ export const PLAYERS: Player[] = [
 	{ id: 0, color: 'red' },
 	{ id: 1, color: 'green' },
 	{ id: 2, color: 'blue' },
-	{ id: 3, color: 'pink' },
+	{ id: 3, color: 'yellow' },
+	{ id: 4, color: 'purple' },
+	{ id: 5, color: 'cyan' },
+	{ id: 6, color: 'orange' },
+	{ id: 7, color: 'white' },
 ]
